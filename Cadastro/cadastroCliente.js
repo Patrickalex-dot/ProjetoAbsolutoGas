27cf8da27851d15abb7fdd5f57ed4c34315e8d18
patrick alexandre <palexandre@live.com>
2022-10-03 22:31:28 -0300

    js

---
+++ Cadastro/cadastroCliente.js
@@ -0,0 +1,13 @@
+function cadastrar() 
+{
+    var nome = document.querySelector("#nome").value;
+    var sobrenome = document.querySelector("#sobrenome").value;
+    var email = document.querySelector("#email").value;
+    var ddd = document.querySelector("#ddd").value;
+    var telefone = document.querySelector("#telefone").value;
+    var tipo = document.querySelector("#tipo").value;
+    var nascimento = document.querySelector("#nascimento").value;
+
+    
+    
+}
